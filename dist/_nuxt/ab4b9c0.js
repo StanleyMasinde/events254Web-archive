ad4856253d6a18cde3b27d687b5608a6a415cfec
Stanley Masinde <stanmasinde@gmail.com>
2021-08-12 17:22:26 +0300

    .gitignore configuration

--- dist/_nuxt/ab4b9c0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[36],{753:function(e,n,t){"use strict";t.r(n);var l={auth:!1},c=t(76),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Events page")])}),[],!1,null,null,null);n.default=component.exports}}]);
