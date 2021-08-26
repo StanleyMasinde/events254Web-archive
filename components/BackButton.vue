4b091960ccda36561d1370cb5288e222015f42d6
Stanley Masinde <stanmasinde@gmail.com>
2021-08-26 11:52:42 +0300

    Added the back button component

---
+++ components/BackButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn icon large @click="goBack">
+    <v-icon>
+      mdi-arrow-left
+    </v-icon>
+  </v-btn>
+</template>
+<script>
+export default {
+  methods: {
+    goBack () {
+      // If window history is available, go back
+      if (window.history && window.history.length > 1) {
+        this.$router.go(-1)
+      } else {
+        // Otherwise, use fallback
+        this.$router.push('/')
+      }
+    }
+  }
+}
+</script>
