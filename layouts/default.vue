62ceb63655cf36a9a1fa3474ff949aa97761a9b5
Stanley Masinde <35426405+StanleyMasinde@users.noreply.github.com>
2021-09-09 12:14:54 +0300

    Bottom Navigation  (#6)
    * Removed the .DS store
    
    * Fixed the organisers string
    
    * Added the back button component
    
    * Added the navigation bar to the group events page
    
    * Added a fallback for the back button
    
    * Added the members sidebar
    
    * Updated the event navigation page
    
    * Using the correct Url for social share
    
    * Fixed the description >>
      * Stripping the HTML tags from it to create friendly descriptions for meta tags
    
    * Fixed the share logic
    
    * Using large summary image for twiiter card
    
    * Upgraded composer dependencies
    
    * Fixed the regression that was causing the rich editor to crush.
     // I hope it's fixed
    
    * Added the bottom navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,46 @@
     <v-main>
       <nuxt />
     </v-main>
+    <v-bottom-navigation
+      v-if="$vuetify.breakpoint.smAndDown"
+      color="primary"
+      fixed
+      app
+      grow
+    >
+      <v-btn
+        icon
+        to="/"
+      >
+        <v-icon>mdi-home-outline</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        :to="`/calendar?date=${today}`"
+      >
+        <v-icon>mdi-calendar-outline</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        to="/home"
+      >
+        <v-icon>mdi-account-outline</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        to="/search"
+      >
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
+<script>
+export default {
+  computed: {
+    today () {
+      return this.$moment().format('YYYY-MM-DD')
+    }
+  }
+}
+</script>
