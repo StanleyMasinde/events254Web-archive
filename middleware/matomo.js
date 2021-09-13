68a6951ea504451b72b72cddf36b1f54fd92c439
Stanley Masinde <stanmasinde@gmail.com>
2021-09-13 23:58:41 +0300

    ft(Analytics): Using user email as user ID

--- middleware/matomo.js
+++ middleware/matomo.js
@@ -7,13 +7,15 @@ export default async function ({ $axios, store, route }) {
   const user = store.state.auth.user
   const _paq = window._paq = window._paq || []
   if (user) {
-    _paq.push(['setUserId', user.id])
+    _paq.push(['setUserId', user.email])
     _paq.push(['setCustomVariable', 1, 'User', user.name, 'visit'])
-    _paq.push(['setCustomVariable', 2, 'User email', user.email, 'visit'])
+    _paq.push(['setCustomVariable', 2, 'email', user.email, 'visit'])
   }
   _paq.push(['trackPageView'])
   _paq.push(['enableLinkTracking'])
   _paq.push(['enableHeartBeatTimer'])
+  _paq.push(['trackAllContentImpressions'])
+  _paq.push(['enableSiteSearch', 'off'])
 
   const u = '//analytics.opensource254.co.ke/'
   _paq.push(['setTrackerUrl', u + 'matomo.php'])
