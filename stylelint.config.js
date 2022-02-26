cf190ab84ee75b969a8e24033ba091db5bbf50a0
Stanley Masinde <stanmasinde@gmail.com>
2022-02-26 12:08:19 +0300

    removed the unknown rule in the stylint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {
-    'unknown-word': [
-      false,
-      'never'
-    ]
-  }
+  rules: {}
 }
