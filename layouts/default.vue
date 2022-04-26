fb9edceaa168e983d26b38ddeae8b4b586e5a57e
Stanley Masinde <stanmasinde@gmail.com>
2022-04-27 01:32:45 +0300

    Added the events

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,15 @@
 <template>
-    <nav class="bg-primary text-white py-5 sm:px-16">
-        <div class="flex justify-between px-1">
-            <h1 class="py-1">Events254</h1>
+    <nav class="bg-white py-3 sm:py-5 px-1 sm:px-16">
+        <div class="flex flex-row-reverse justify-between px-1">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
             <div class="sm:flex">
                 <button @click="toggleMenu" class="block sm:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-8" fill="none" viewBox="0 0 24 24"
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor" stroke-width="2">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h8m-8 6h16" />
                     </svg>
                 </button>
 
@@ -21,7 +24,7 @@
 
                     <router-link to="/register">
                         <li>
-                            <button class="py-1 px-3 rounded-md border border-white hover:bg-secondary">
+                            <button class="py-1 px-3 rounded-md border hover:bg-primary">
                                 Register
                             </button>
                         </li>
@@ -31,28 +34,29 @@
         </div>
 
         <!-- Mobile menu -->
-        <div :class="{ 'hidden': mobileMenuToggle }" class="transition ease-in sm:hidden">
-            <ul class="mt-4">
-                <router-link to="/explore">
-                    <li class="rounded px-1 py-1">Explore</li>
-                </router-link>
+            <div :class="{ 'hidden': mobileMenuToggle }"
+                class="relative z-10 shadow sm:hidden">
+                <ul class="mt-4 pb-2 rounded-md bg-white absolute w-full">
+                    <router-link to="/explore">
+                        <li class="rounded px-1 py-1">Explore</li>
+                    </router-link>
 
-                <router-link to="/login">
-                    <li class="px-1 py-1 mt-1 flex justify-between">
-                        Login
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
-                        </svg>
-                    </li>
-                </router-link>
+                    <router-link to="/login">
+                        <li class="px-1 py-1 mt-1 flex justify-between">
+                            Login
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                            </svg>
+                        </li>
+                    </router-link>
 
-                <router-link to="/register">
-                    <li class="rounded-md px-1 py-1 mt-1">Register</li>
-                </router-link>
-            </ul>
-        </div>
+                    <router-link to="/register">
+                        <li class="rounded-md px-1 py-1 mt-1">Register</li>
+                    </router-link>
+                </ul>
+            </div>
         <!--/ Mobile menu -->
 
     </nav>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,75 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello {{ name }}!ß
-  </h1>
+  <!-- header/Hero -->
+  <section class="bg-slate-500 h-64 mb-7 relative">
+
+    <div class="absolute w-full bottom-0 px-2 sm:px-16 -mb-7">
+      <input class="form-input w-full h-14 rounded-lg" placeholder="Search for events, locations dates etc" />
+    </div>
+  </section>
+  <!-- Header/hero -->
+
+  <!-- Events -->
+  <section class="mt-16">
+    <!-- <div v-for="(e, i) in data" :key="i" class="border rounded-md grid grid-cols-3 grid-flow-col mb-2 mx-1">
+      <div class="mr-2">
+        <img class=" rounded-l-md h-32 w-full" :src="e.image">
+      </div>
+      <div class="col-span-2">
+        <h1 class="text-lg font-bold">{{ e.title }}</h1>
+        <p class="line-clamp-4 text-sm">{{ e.description }}</p>
+      </div>
+    </div> -->
+
+    <!-- Event types -->
+    <div v-for="(s, i) in data" :key="i" class="mb-7">
+      <div v-if="s.data.length > 0" class="flex justify-between mx-2">
+        <h1>{{ s.name }}</h1>
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+          stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+        </svg>
+      </div>
+
+      <div v-if="s.data.length > 0" class="flex overflow-y-hidden ml-2 rounded-md snap-mandatory snap-x">
+
+        <div class="h-40 min-w-[97vw] max-w-[90vw] pr-1 rounded-md grid grid-cols-2 snap-center"
+          v-for="(e, i) in s.data" :key="i">
+
+          <div>
+            <img class="w-full h-full object-cover object-center rounded-md"
+              :src="e.pictureUrl ? e.pictureUrl : 'https://picsum.photos/id/1/200/300'">
+          </div>
+          <div class=" grid grid-rows-5">
+            <div>
+              <h1 class="line-clamp-2 font-bold text-sm">{{ e.name }}</h1>
+              <p class=" line-clamp-3 text-sm">{{ e.description }}</p>
+            </div>
+
+            <div class="row-span-4">
+              <p class=" text-red-500 text-sm" v-if="e.linkPrefix == 'events'">{{ moment(e.startDate).calendar(null,
+                  {
+                    sameElse: 'MMM DD, [from] hh:mm A'
+                  })
+              }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Event Types -->
+  </section>
+  <!-- Events -->
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import moment from 'moment-timezone'
 
+const { data, pending, error, refresh } = await useAsyncData(
+  'mountains',
+  () => $fetch(`${process.env.API_URL}/feed`)
+)
 
-definePageMeta({
-  title: 'Events254'
-})
-const name = ref('John');
-const message = ref('Hello Doe');
+console.log(data);
+console.log(error);
+console.log();
 </script>
