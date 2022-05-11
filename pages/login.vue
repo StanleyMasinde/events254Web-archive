143d32b7a7dacf630f40e76a298afdd828bc6ef6
Stanley Masinde <stanmasinde@gmail.com>
2022-05-11 18:03:46 +0300

    Added the login and registration pages mockups

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,40 @@
 <template>
-    <h1>Login Page</h1>
+    <section class="px-2">
+        <div>
+            <div class="mb-4 mt-2 text-center">
+                <h1 class="text-xl font-bold">Welcome back!</h1>
+                <p class="mt-2 mb-7">Enter your account details to sign in into your Events254 account</p>
+            </div>
+            <form method="POST">
+                <label for="username">
+                    <h1>Email, phone or username</h1>
+                    <input class="w-full rounded-lg mb-2" type="text" name="username" id="username"
+                        placeholder="Username, email or phone">
+                </label>
+
+                <label for="password">
+                    <h1>Password</h1>
+                    <input class="w-full rounded-lg mb-3" type="password" name="password" id="password"
+                        placeholder="Password">
+                </label>
+
+                <div class="my-4">
+                    <h1 class="underline">Having trouble signing in?</h1>
+                </div>
+
+
+                <button class="bg-primary rounded-lg w-full text-white font-bold py-2 px-4">
+                    Sign in
+                </button>
+            </form>
+
+            <div class=" mt-8 text-center">
+                <nuxt-link to="/register">
+                    <h1 class="underline">Don't have an account? Create one</h1>
+                </nuxt-link>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script setup>
--- pages/register.vue
+++ pages/register.vue
@@ -1,5 +1,40 @@
 <template>
-    <h1>Register Page</h1>
+    <section class="p-3">
+        <div class="text-center mt-4">
+            <h1 class="text-2xl font-bold">Sign Up</h1>
+            <p>Create your free Events254 account today</p>
+        </div>
+
+        <div>
+            <form action="#" method="post">
+                <label for="name">
+                    <h1 class="mt-5">Your full name<span class=" text-red-400">*</span></h1>
+                    <input class="w-full rounded-lg" type="text" name="name" id="name" placeholder="John Doe">
+                </label>
+
+                <label for="email">
+                    <h1 class="mt-3">Email<span class=" text-red-400">*</span></h1>
+                    <input class="w-full rounded-lg" type="text" name="email" id="email" placeholder="john@example.com">
+                </label>
+
+                <label for="password">
+                    <h1 class="mt-3">Choose a strong password<span class="text-red-400">*</span></h1>
+                    <input class="w-full rounded-lg" type="text" name="name" id="name"
+                        placeholder="At least 8 characters">
+                </label>
+
+                <div class="px-2">
+                    <button class="w-full bg-primary text-white rounded-lg mt-5 py-2">Create account</button>
+                </div>
+            </form>
+
+            <div class="mt-5 text-center">
+                <nuxt-link to="/login">
+                    <h1 class="underline">Have an account?</h1>
+                </nuxt-link>
+            </div>
+        </div>
+    </section>
 </template>
 <script setup>
 useHead({
