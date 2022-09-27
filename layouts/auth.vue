078acf5fcc811e57da9aa6456789315020178112
Stanley Masinde <stanmasinde@gmail.com>
2022-09-27 22:07:44 +0300

    fix the password reset issue

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class=" h-screen bg-[url('/authbg.svg')] bg-no-repeat bg-cover bg-center grid place-items-center">
+        <div class="h-screen bg-[url('/authbg.svg')] bg-no-repeat bg-cover bg-center grid place-items-center">
             <slot></slot>
         </div>
     </div>
