11fc9a148de5ab1d7d2ebc67385687b1898a2a92
Stanley Masinde <stanmasinde@gmail.com>
2022-09-28 12:00:19 +0300

    use runtime config to get the environment and release

--- plugins/sentry.client.ts
+++ plugins/sentry.client.ts
@@ -6,11 +6,12 @@ export default defineNuxtPlugin((nuxtApp) => {
     name: localStorage.getItem('auth.name'),
     email: localStorage.getItem('auth.email'),
   }
+  const { public: { environment, release } } = useRuntimeConfig()
   Sentry.init({
     app: vueApp,
     dsn: "https://a500da3ef9364208950c29c41ed9b619@o954334.ingest.sentry.io/6778723",
-    environment: process.env.NODE_ENV,
-    release: process.env.npm_package_version,
+    environment,
+    release,
     integrations: [
       new BrowserTracing({
         tracingOrigins: ["events254.co.ke", /^\//],
