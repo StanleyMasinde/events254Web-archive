7ea8ae43caadc4d5f3f0065675ccbeb52f19a476
Stanley Masinde <stanmasinde@gmail.com>
2022-09-28 11:46:35 +0300

    updated sentry connfig * Add user? * reduce the sample rate to 20% * on do tracing in production * Add releases * Add environment

--- plugins/sentry.client.ts
+++ plugins/sentry.client.ts
@@ -1,18 +1,26 @@
 import * as Sentry from "@sentry/vue";
 import { BrowserTracing } from "@sentry/tracing";
 export default defineNuxtPlugin((nuxtApp) => {
-    const vueApp = nuxtApp.vueApp
-    Sentry.init({
-        app: vueApp,
-        dsn: "https://a500da3ef9364208950c29c41ed9b619@o954334.ingest.sentry.io/6778723",
-        integrations: [
-            new BrowserTracing({
-              tracingOrigins: ["events254.test", "events254.co.ke", /^\//],
-            }),
-          ],
-          // Set tracesSampleRate to 1.0 to capture 100%
-          // of transactions for performance monitoring.
-          // We recommend adjusting this value in production
-          tracesSampleRate: 1.0,
-    })
+  const vueApp = nuxtApp.vueApp
+  const user = {
+    name: localStorage.getItem('auth.name'),
+    email: localStorage.getItem('auth.email'),
+  }
+  Sentry.init({
+    app: vueApp,
+    dsn: "https://a500da3ef9364208950c29c41ed9b619@o954334.ingest.sentry.io/6778723",
+    environment: process.env.NODE_ENV,
+    release: process.env.npm_package_version,
+    integrations: [
+      new BrowserTracing({
+        tracingOrigins: ["events254.co.ke", /^\//],
+      }),
+    ],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.2,
+  })
+
+  Sentry.setUser(user)
 })
